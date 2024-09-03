9bb1bd6cc360d9ff818d545a6521ec2e73e655f9
hjalmar <venondesign@gmail.com>
2024-09-03 15:37:51 +0200

    output torch files and adjust models

--- data/generate_data.py
+++ data/generate_data.py
@@ -6,9 +6,24 @@ import random
 from scipy.ndimage import gaussian_filter
 from sklearn.model_selection import StratifiedShuffleSplit
 
-from common import DataRecord, DataScenarios, SEED
-from utils import load_json_file, dump_as_pickle
-from data.data_utils import generate_backgrounds, generate_imagenet, generate_fixed, generate_translations_rotations, generate_xor, normalise_data, scale_to_bound
+from xai_master.scenarios.xai_tris.xai_tris_repo.common import (
+    DataRecord,
+    DataScenarios,
+    SEED,
+)
+from xai_master.scenarios.xai_tris.xai_tris_repo.utils import (
+    load_json_file,
+    dump_as_pickle,
+)
+from xai_master.scenarios.xai_tris.xai_tris_repo.data.data_utils import (
+    generate_backgrounds,
+    generate_imagenet,
+    generate_fixed,
+    generate_translations_rotations,
+    generate_xor,
+    normalise_data,
+    scale_to_bound,
+)
 
 from datetime import datetime
 from pathlib import Path
@@ -16,77 +31,103 @@ from pathlib import Path
 import os
 import sys
 
-os.environ['PYTHONHASHSEED']=str(SEED)
+os.environ["PYTHONHASHSEED"] = str(SEED)
 random.seed(SEED)
 np.random.seed(SEED)
 
 scenario_dict = {
-    'linear': generate_fixed,
-    'multiplicative': generate_fixed,
-    'translations_rotations': generate_translations_rotations,
-    'xor': generate_xor
+    "linear": generate_fixed,
+    "multiplicative": generate_fixed,
+    "translations_rotations": generate_translations_rotations,
+    "xor": generate_xor,
 }
 
 
-def data_generation_process(config: Dict, output_dir: str) -> List:
+def data_generation_process(config: Dict, output_dir: str):
     # experiment_list = []
-    image_shape = np.array(config["image_shape"])*config["image_scale"]
-    for i in range(config['num_experiments']): # generate multiple datasets if desired
-        
-        backgrounds = generate_backgrounds(config['sample_size'], config['mean_data'], config['var_data'], image_shape )
+    image_shape = np.array(config["image_shape"]) * config["image_scale"]
+    for i in range(config["num_experiments"]):  # generate multiple datasets if desired
+        backgrounds = generate_backgrounds(
+            config["sample_size"], config["mean_data"], config["var_data"], image_shape
+        )
         if config["use_imagenet"]:
-            imagenet_backgrounds = generate_imagenet(config['sample_size'])
-        
+            imagenet_backgrounds = generate_imagenet(config["sample_size"])
+
         # Iterate over config
         k = 0
-        for scenario, params in config['parameterizations'].items():
-            for pattern_scale in params['pattern_scales']:
-                config['pattern_scale'] = pattern_scale
-                patterns = scenario_dict.get(scenario)(params=config, image_shape=list(image_shape))
+        for scenario, params in config["parameterizations"].items():
+            for pattern_scale in params["pattern_scales"]:
+                config["pattern_scale"] = pattern_scale
+                patterns = scenario_dict.get(scenario)(
+                    params=config, image_shape=list(image_shape)
+                )
                 ground_truths = patterns.copy()
 
-                for correlated in ['white', 'correlated', 'imagenet']:
-                    if correlated == 'imagenet' and not config["use_imagenet"]:
+                for correlated in ["white", "correlated", "imagenet"]:
+                    if correlated == "imagenet" and not config["use_imagenet"]:
                         continue
-                    copy_backgrounds = np.zeros((config['sample_size'], image_shape[0]*image_shape[1]))
-                    params['correlated_background'] = correlated
-                    if correlated == 'correlated':
+                    copy_backgrounds = np.zeros(
+                        (config["sample_size"], image_shape[0] * image_shape[1])
+                    )
+                    params["correlated_background"] = correlated
+                    if correlated == "correlated":
                         for j, background in enumerate(backgrounds.copy()):
-                            copy_backgrounds[j] = gaussian_filter(np.reshape(background, (image_shape[0],image_shape[1])), config['smoothing_sigma']).reshape((image_shape[0]*image_shape[1]))
+                            copy_backgrounds[j] = gaussian_filter(
+                                np.reshape(
+                                    background, (image_shape[0], image_shape[1])
+                                ),
+                                config["smoothing_sigma"],
+                            ).reshape((image_shape[0] * image_shape[1]))
                         alpha_ind = 1
-                    elif correlated == 'imagenet' and config["use_imagenet"]:
+                    elif correlated == "imagenet" and config["use_imagenet"]:
                         copy_backgrounds = imagenet_backgrounds.copy()
                         alpha_ind = 2
                     else:
                         copy_backgrounds = backgrounds.copy()
                         alpha_ind = 0
 
-                    for alpha in params['snrs'][alpha_ind]:
-                        scenarios = type(DataScenarios)()
-                        print(f'Generating scenario {scenario} with {correlated} background and alpha={alpha}')
+                    for alpha in params["snrs"][alpha_ind]:
+                        print(
+                            f"Generating scenario {scenario} with {correlated} background and alpha={alpha}"
+                        )
                         copy_patterns = patterns.copy()
-                        if params['manipulation_type'] == 'multiplicative':                        
+                        if params["manipulation_type"] == "multiplicative":
                             copy_patterns = 1 - alpha * copy_patterns
 
-                        normalised_patterns, normalised_backgrounds = normalise_data(copy_patterns, copy_backgrounds)
+                        normalised_patterns, normalised_backgrounds = normalise_data(
+                            copy_patterns, copy_backgrounds
+                        )
 
-                        if params['manipulation_type'] == 'multiplicative':
+                        if params["manipulation_type"] == "multiplicative":
                             x = normalised_patterns * normalised_backgrounds
                         else:
-                            x = alpha * normalised_patterns.copy() + (1 - alpha) * normalised_backgrounds.copy()
-                    
+                            x = (
+                                alpha * normalised_patterns.copy()
+                                + (1 - alpha) * normalised_backgrounds.copy()
+                            )
+
                         scale = 1 / np.max(np.abs(x))
                         x = np.apply_along_axis(scale_to_bound, 1, x, scale)
 
-                        class_0_labels = torch.as_tensor([[0]] * int(config['sample_size']/len(config['patterns'])))
-                        class_1_labels = torch.as_tensor([[1]] * int(config['sample_size']/len(config['patterns'])))
+                        class_0_labels = torch.as_tensor(
+                            [[0]] * int(config["sample_size"] / len(config["patterns"]))
+                        )
+                        class_1_labels = torch.as_tensor(
+                            [[1]] * int(config["sample_size"] / len(config["patterns"]))
+                        )
 
                         x = torch.as_tensor(x, dtype=torch.float16)
-                        y = torch.ravel(torch.cat((class_0_labels,class_1_labels)))
+                        y = torch.ravel(torch.cat((class_0_labels, class_1_labels)))
 
                         # 90/5/5 split for 64x64 data, 80/10/10 split for 8x8
-                        data_splitter = StratifiedShuffleSplit(n_splits=1, test_size=config['test_split'], random_state=SEED)
-                        train_indices, val_indices = list(data_splitter.split(X=x, y=y))[0]
+                        data_splitter = StratifiedShuffleSplit(
+                            n_splits=1,
+                            test_size=config["test_split"],
+                            random_state=SEED,
+                        )
+                        train_indices, val_indices = list(
+                            data_splitter.split(X=x, y=y)
+                        )[0]
 
                         x_train = x[train_indices]
                         y_train = y[train_indices]
@@ -96,41 +137,58 @@ def data_generation_process(config: Dict, output_dir: str) -> List:
                         masks_train = ground_truths[train_indices]
                         masks_val_test = ground_truths[val_indices]
 
-                        data_splitter = StratifiedShuffleSplit(n_splits=1, test_size=0.5, random_state=SEED)
-                        val_indices, test_indices = list(data_splitter.split(X=x_val_test, y=y_val_test))[0]
+                        data_splitter = StratifiedShuffleSplit(
+                            n_splits=1, test_size=0.5, random_state=SEED
+                        )
+                        val_indices, test_indices = list(
+                            data_splitter.split(X=x_val_test, y=y_val_test)
+                        )[0]
 
                         x_val = x_val_test[val_indices]
                         y_val = y_val_test[val_indices]
                         masks_val = masks_val_test[val_indices]
-                        
+
                         x_test = x_val_test[test_indices]
                         y_test = y_val_test[test_indices]
                         masks_test = masks_val_test[test_indices]
 
                         correlated_string = "uncorrelated"
-                        if correlated == 'imagenet':
-                            correlated_string = 'imagenet'
-                        elif correlated == 'correlated':
+                        if correlated == "imagenet":
+                            correlated_string = "imagenet"
+                        elif correlated == "correlated":
                             correlated_string = "correlated"
-                        
+
                         scenario_key = f'{params["scenario"]}_{config["image_scale"]}d{config["pattern_scale"]}p_{alpha}_{correlated_string}'
 
-                        scenarios[scenario_key] = DataRecord(x_train, y_train, x_val, y_val, x_test, y_test, masks_train, masks_val, masks_test)
+                        scenario = {
+                            "key": scenario_key,
+                            "x_train": x_train,
+                            "y_train": y_train,
+                            "x_val": x_val,
+                            "y_val": y_val,
+                            "x_test": x_test,
+                            "y_test": y_test,
+                            "masks_train": masks_train,
+                            "masks_val": masks_val,
+                            "masks_test": masks_test,
+                        }
 
-                        dump_as_pickle(data=scenarios, output_dir=output_dir, file_name=scenario_key)
+                        # Save as torch dict
+                        torch.save(scenario, f"{output_dir}/{scenario_key}.pt")
+
 
 def main():
-    config_file = 'data_config'
-    if len (sys.argv) > 1:
+    config_file = "data_config"
+    if len(sys.argv) > 1:
         config_file = sys.argv[1]
-    config = load_json_file(file_path=f'data/{config_file}.json') 
-    
+    config = load_json_file(file_path=f"data/{config_file}.json")
+
     date = datetime.now().strftime("%Y-%m-%d-%H-%M")
     folder_path = f'{config["output_dir"]}/{date}'
     Path(folder_path).mkdir(parents=True, exist_ok=True)
-    
+
     data_generation_process(config=config, output_dir=folder_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- training/models.py
+++ training/models.py
@@ -1,8 +1,10 @@
 from torch.nn import Linear, ReLU, Sequential, Conv2d, MaxPool2d, Module, Dropout
 from torch import softmax
 from torch.nn.init import kaiming_normal_
+import torch
 
-class CNN(Module):   
+
+class CNN(Module):
     def __init__(self, n_dim, linear_dim):
         super(CNN, self).__init__()
         self.n_dim = n_dim
@@ -26,52 +28,53 @@ class CNN(Module):
             ReLU(),
             MaxPool2d(kernel_size=2),
         )
-        
-        self.linear_layers = Sequential(
-            Linear(self.linear_dim, 2, bias=False)
-        )
 
-    # Defining the forward pass    
+        self.linear_layers = Sequential(Linear(self.linear_dim, 2, bias=False))
+
+    # Defining the forward pass
     def forward(self, x):
         x = self.cnn_layers(x)
         x = x.view(x.size(0), -1)
         x = softmax(self.linear_layers(x), dim=1)
         return x
 
-class LLR(Module):   
+
+class LLR(Module):
     def __init__(self, n_dim):
         super(LLR, self).__init__()
         self.linear = Linear(n_dim, 2, bias=False)
 
-    # Defining the forward pass    
+    # Defining the forward pass
     def forward(self, x):
         x = softmax(self.linear(x), dim=1)
         return x
 
-class MLP(Module):   
+
+class MLP(Module):
     def __init__(self, n_dim):
         super(MLP, self).__init__()
         self.n_dim = n_dim
 
         self.linear_layers = Sequential(
-            Linear(self.n_dim, int(256),  bias=False),
+            Linear(self.n_dim, int(256), bias=False),
             ReLU(),
             Dropout(p=0.3),
-            Linear(int(256), int(256),  bias=False),
+            Linear(int(256), int(256), bias=False),
             ReLU(),
             Dropout(p=0.3),
-            Linear(int(256), int(64),  bias=False),
+            Linear(int(256), int(64), bias=False),
             ReLU(),
             Dropout(p=0.3),
-            Linear(int(64),2, bias=False)
+            Linear(int(64), 2, bias=False),
         )
 
-    # Defining the forward pass    
+    # Defining the forward pass
     def forward(self, x):
         x = softmax(self.linear_layers(x), dim=1)
         return x
 
-class CNN8by8(Module):   
+
+class CNN8by8(Module):
     def __init__(self, n_dim, linear_dim):
         super(CNN8by8, self).__init__()
         self.n_dim = n_dim
@@ -95,47 +98,71 @@ class CNN8by8(Module):
             ReLU(),
             MaxPool2d(kernel_size=2, stride=2),
         )
-        
-        self.linear_layers = Sequential(
-            Linear(self.linear_dim, 2)
-        )
 
-    # Defining the forward pass    
+        self.linear_layers = Sequential(Linear(self.linear_dim, 2))
+
+    # Defining the forward pass
     def forward(self, x):
         x = self.cnn_layers(x)
         x = x.view(x.size(0), -1)
         x = softmax(self.linear_layers(x), dim=1)
         return x
 
-class MLP8by8(Module):   
+    def get_layers(self):
+        return list(self.cnn_layers.children()) + list(self.linear_layers.children())
+
+    def collect_activations(self, x):
+        yield x, 0, "input"
+        layers = self.get_layers()
+        cur = x
+        with torch.no_grad():
+            for idx, layer in enumerate(layers):
+                cur = layer(cur)
+                yield cur, idx + 1, str(layer)
+
+
+class MLP8by8(Module):
     def __init__(self, n_dim):
         super(MLP8by8, self).__init__()
         self.n_dim = n_dim
 
         self.linear_layers = Sequential(
-            Linear(self.n_dim, int(self.n_dim/2)),
+            Linear(self.n_dim, int(self.n_dim / 2)),
             ReLU(),
-            Linear(int(self.n_dim/2), int(self.n_dim/4)),
+            Linear(int(self.n_dim / 2), int(self.n_dim / 4)),
             ReLU(),
-            Linear(int(self.n_dim/4), int(self.n_dim/8)),
+            Linear(int(self.n_dim / 4), int(self.n_dim / 8)),
             ReLU(),
-            Linear(int(self.n_dim/8),2)
+            Linear(int(self.n_dim / 8), 2),
         )
 
-    # Defining the forward pass    
+    # Defining the forward pass
     def forward(self, x):
         x = softmax(self.linear_layers(x), dim=1)
         return x
 
+    def get_layers(self):
+        return list(self.linear_layers.children())
+
+    def collect_activations(self, x):
+        yield x, 0, "input"
+        layers = self.get_layers()
+        cur = x
+        with torch.no_grad():
+            for idx, layer in enumerate(layers):
+                cur = layer(cur)
+                yield cur, idx + 1, str(layer)
+
 
 def init_he_normal(layer):
-    if isinstance(layer, Conv2d) or isinstance(layer,Linear):
+    if isinstance(layer, Conv2d) or isinstance(layer, Linear):
         kaiming_normal_(layer.weight)
+
 
 models_dict = {
     "CNN": CNN,
     "LLR": LLR,
     "MLP": MLP,
     "CNN8by8": CNN8by8,
-    "MLP8by8": MLP8by8
+    "MLP8by8": MLP8by8,
 }
